1fd90ba9782731d59de93bf89e29f6e8721c53f9
Kayla Hansen <shiverr@gmail.com>
2017-10-24 02:14:53 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -14,7 +14,7 @@ class Triangle
       raise TriangleError
     end
 
-    if (@side_1 + @side_2) < @side_3 || (@side_2 + @side_3) < @side_1 || (@side_1 + @side_3) < @side_2
+    if (@side_1 + @side_2) <= @side_3 || (@side_2 + @side_3) <= @side_1 || (@side_1 + @side_3) <= @side_2
       raise TriangleError
     end
 
